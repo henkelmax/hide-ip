d29faac9a0c8dd6b7011f332ad94bb6db393b107
Max Henkel <max@maxhenkel.de>
2021-11-11 21:31:04 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# HideIP
+
+Hides your servers IP address in disconnection messages.
+
+![](https://user-images.githubusercontent.com/13237524/141364391-39b0899c-1409-4231-a264-05290593ff15.png)
---
+++ src/main/java/de/maxhenkel/hideip/HideIP.java
@@ -0,0 +1,16 @@
+package de.maxhenkel.hideip;
+
+import net.fabricmc.api.ClientModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HideIP implements ClientModInitializer {
+
+    public static final String MODID = "hideip";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
